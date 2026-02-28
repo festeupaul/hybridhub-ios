791542b046f2db1c484de345f1a385f4e6c38be9
Paul Festeu <festeu1610@gmail.com>
2026-02-28 20:01:59 +0200

    Initial Commit

---
+++ HybridHub/HybridHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HybridHubApp.swift
+//  HybridHub
+//
+//  Created by Paul Festeu on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct HybridHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
